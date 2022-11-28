ab71bd6edca36df308253b0d914ba64b77086c07
Raul Victor <31050006+Raul772@users.noreply.github.com>
2022-11-28 17:57:14 -0300

    Update: Add include employee feature

--- src/entities/Administrador.java
+++ src/entities/Administrador.java
@@ -1,5 +1,9 @@
-
 package entities;
+
+import com.google.gson.Gson;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.io.IOException;
 
 public class Administrador extends Pessoa{
     
@@ -29,6 +33,27 @@ public class Administrador extends Pessoa{
     
     public long getIdAdministrador() {
         return idAdministrador;
+    }
+    
+    public Funcionario incluirFuncionario(long id, String nome, String cpf) throws IOException{
+        Path filePath = Path.of("C:","ProjetoLanchonete",
+                            "registros", "funcionarios", 
+                            String.format("%s.json", id));
+        
+        Gson gson = new Gson();
+        
+        if(!Files.exists(filePath.getParent()))
+            Files.createDirectories(filePath.getParent());
+        
+        if(Files.exists(filePath)){ 
+            Funcionario funcionario = 
+                    gson.fromJson(Files.readString(filePath), Funcionario.class);
+            return funcionario;
+        }
+        
+        Funcionario funcionario = new Funcionario(id, nome, cpf);
+        Files.writeString(filePath, gson.toJson(funcionario));
+        return funcionario;
     }
     
     @Override
--- src/projetolanchonete/ProjetoLanchonete.java
+++ src/projetolanchonete/ProjetoLanchonete.java
@@ -3,20 +3,31 @@ package projetolanchonete;
 import entities.Administrador;
 import entities.Cliente;
 import entities.Funcionario;
+import java.io.IOException;
 
 public class ProjetoLanchonete {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
        
-        Funcionario a = new Funcionario(0, "Laura", "43256709856");
-        Cliente b = new Cliente(0, "Rua xuxa", "40028922", "Júlia", "12345678990");
-        Administrador c = new Administrador(0, "Bianca", "09876543210");
+        
+        Cliente a = new Cliente(0, "Rua xuxa", "40028922", "Júlia", "12345678990");
+        Administrador b = new Administrador(0, "Bianca", "09876543210");
+        Funcionario c = b.incluirFuncionario(0, "Laura", "423424234346");
+        Funcionario d = b.incluirFuncionario(1, "Bianca", "12312313236");
+        Funcionario e = b.incluirFuncionario(2, "Larissa", "4323454656");
+        Funcionario f = b.incluirFuncionario(3, "Xuxa Meneghel", "43378463746");
         
         System.out.println(a);
         System.out.println("\n");
         System.out.println(b);
         System.out.println("\n");
         System.out.println(c);
+        System.out.println("\n");
+        System.out.println(d);
+        System.out.println("\n");
+        System.out.println(e);
+        System.out.println("\n");
+        System.out.println(f);
        
     }
     
